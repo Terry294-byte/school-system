1ed32fab321411e9921f58799664660752607ecf
Terry294-byte <shiroterry168@gmail.com>
2025-02-05 23:14:55 +0300

    added the node_module

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Login = ({ setToken }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/login", { username, password });
+      setToken(response.data.token);
+    } catch (error) {
+      alert("Invalid login");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Teacher Login</h2>
+      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
